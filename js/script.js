1b02e471ac580d586bf655205e17eb8a1db43a09
Felipe <118012333+FelipePaterni@users.noreply.github.com>
2025-04-20 16:17:41 -0300

    closes #4 -Add quantity management functionality and update calculation logic

--- js/script.js
+++ js/script.js
@@ -1,21 +1,38 @@
 //mascara de telefone
 $("#phone").mask("(99) 9999-99999");
 
-
+//lista de produtos
+var prods = [
+  { id: 1, name: "Batata com bife", price: 30.0 },
+  { id: 2, name: "Coxa de Frango Crocante", price: 25.0 },
+  { id: 3, name: "Carne de Panela", price: 22.0 },
+  { id: 4, name: "Farofa", price: 10.0 },
+  { id: 5, name: "Salada", price: 8.0 },
+  { id: 6, name: "Torresmo", price: 12.0 },
+];
 
 function calc() {
+  //inputs
   var nome = document.getElementById("name").value;
-  var telefone = document.getElementById("phone").value;
-  var email = document.getElementById("email").value;
 
-  output = document.getElementById("output");
+  var quantities = document.getElementsByName("quantity");
+  var total = 0;
+
+  var output = document.getElementById("output");
   output.innerHTML = "";
 
   var formatter = new Intl.NumberFormat("pt-BR", {
     style: "currency",
     currency: "BRL",
-
-
-
   });
+
+  for (var input of quantities) {
+    var id = input.id;
+    if (input.value > 0) {
+      output.innerHTML += `Produto: ${prods[id - 1].name}  - Preço: ${
+        prods[id - 1].price
+      } - Quantidade: ${input.value} </br>`;
+      total += prods[id - 1].price * parseFloat(input.value);
+    }
+  }
 }
